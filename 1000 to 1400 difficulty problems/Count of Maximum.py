e3335d32bf0a8d93dcf22f47c58d1b225a9daf22
K.Meghana <kmegha9505@gmail.com>
2025-06-30 15:28:10 +0530

    Done Count of Maximum.py

---
+++ 1000 to 1400 difficulty problems/Count of Maximum.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+    freq = Counter(A)
+    
+    max_count = max(freq.values())
+    min_val = min(k for k, v in freq.items() if v == max_count)
+    
+    print(min_val, max_count)
