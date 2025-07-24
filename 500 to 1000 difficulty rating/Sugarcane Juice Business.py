212314d7ae72b9a4b9281f8451c63f54dcbb96a2
K.Meghana <kmegha9505@gmail.com>
2025-07-24 14:19:04 +0530

    Create Sugarcane Juice Business.py

---
+++ 500 to 1000 difficulty rating/Sugarcane Juice Business.py
@@ -0,0 +1,6 @@
+T = int(input())  # Number of test cases
+
+for _ in range(T):
+    N = int(input())  # Number of glasses sold
+    profit = N * 15   # 30% of 50 = 15 coins profit per glass
+    print(profit)
